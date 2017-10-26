a3a409c41600b5cee8950108e201458e15d9f111
Bryt12 <brysontang@gmail.com>
2017-10-25 22:56:43 -0400

    Constructed constructor for Moment class

--- src/Moment.java
+++ src/Moment.java
@@ -5,6 +5,10 @@ public class Moment {
     private map<Account, int> participants
 
     public Moment (String name, Image image, ArrayList participants, ArrayList smileValues){
-
+        this.name = name;
+        this.image = image;
+        for(int i = 0; i < participants.length; i ++){
+            this.participants.put(participants.get(i), smileValues.get(i));
+        }
     }
 }
