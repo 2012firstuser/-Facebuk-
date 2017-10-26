0002a60c8c5b4276af03b15b17c90c8f99206382
Ankur Gupta <rukna1000@gmail.com>
2017-10-25 23:22:22 -0400

    Added the attributes for the Account class

--- src/Account.java
+++ src/Account.java
@@ -3,5 +3,11 @@
     Todo: Class Info
  */
 
+import java.util.ArrayList;
+
 public abstract class Account {
+    private String name;
+    private Image image;
+    private ArrayList<Account> friends;
+    private ArrayList<Moment> moments;
 }
