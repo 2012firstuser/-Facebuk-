25c17d5b865176b5160a2284ec7ec5f0ea51239e
Ankur Gupta <rukna1000@gmail.com>
2017-10-30 01:34:07 -0400

    Added documentation for Account. @bryt12 needs to doc 2 functions, then we should be all good.

--- src/Account.java
+++ src/Account.java
@@ -3,29 +3,59 @@ import java.util.HashMap;
 
 
 /**
+ * Abstract Class Account
  *
+ * We created this class because a Person and a Pet are effectively the same
+ * object, with the exception being that a Person can have a Pet, and that
+ * a Pet has a Person as an owner.
+ *
+ * Because of that situation, it would be inefficient to copy the similar
+ * methods and attributes to both Person and Pet, so this class is a way
+ * to modularize the program a bit more.
  */
 
 public abstract class Account extends Page {
     private ArrayList<Account> _friends;
     private ArrayList<Moment> _moments;
 
+    /**
+     * Retrieve the moments of a child of Account
+     * @return an {@link ArrayList<Moment>} of all of the moments of the
+     *         child of Account
+     */
     public ArrayList<Moment> getMoments () {
         return this._moments;
     }
 
+    /**
+     * Retrieve the friends of a child of Account
+     * @return an {@link ArrayList<Account>} of all of the friends (Persons /
+     *         Pets) of the child of Account
+     */
     public ArrayList<Account> getFriends() {
         return this._friends;
     }
 
+    /**
+     * Set the friends of a child of Account
+     * @param friends the new list of friends for the child of Account
+     */
     public void setFriends (ArrayList<Account> friends) {
         this._friends = friends;
     }
 
+    /**
+     * Set the moments of a child of Account
+     * @param moments the new list of moments for the child of Account
+     */
     public void setMoments (ArrayList<Moment> moments) {
         this._moments = moments;
     }
 
+    /**
+     * TODO: Bryson, take care of this one :P
+     * @return
+     */
     public Account getFriendWithWhomIAmHappiest (){
         HashMap<Account, Float> friendMap = new HashMap<>();
         HashMap<Account, Integer> friendCount = new HashMap<>();
@@ -53,6 +83,10 @@ public abstract class Account extends Page {
         return happiestFriend;
     }
 
+    /**
+     * TODO: Bryson, take care of this one :P :P
+     * @return
+     */
     public Moment getOverallHappiestMoment (){
         Moment happiestMoment = null;
 
@@ -65,6 +99,11 @@ public abstract class Account extends Page {
         return happiestMoment;
     }
 
+    /**
+     * TODO: Bryson, take care of this one :P :P: :P
+     * @param m
+     * @return
+     */
     protected float momentMeanValue(Moment m){
         float totalHappiness = 0;
         HashMap<Account, Float> parsMap = m.getParticipants();
@@ -76,10 +115,30 @@ public abstract class Account extends Page {
         return totalHappiness / parsMap.size();
     }
 
+    /**
+     * Determines the maximum clique of friends for a single person. This is just a
+     * trampoline for the recursive method {@see Account#analyzeNode} which determines
+     * if each friend of an Account is part of the maximum clique
+     *
+     * @return  the biggest {@link ArrayList<Account>} of Accounts in which everybody
+     *          is a friend with one another.
+     */
     public ArrayList<Account> findMaximumCliqueOfFriends () {
         return this.analyzeNode(this, new ArrayList<Account>());
     }
 
+    /**
+     * Determines if the current Account part of the biggest Clique. Given the
+     * current Account and the visited Accounts, will check if all of the friends
+     * of the current Account are in the visited Accounts.
+     *
+     * If that is the case, then the function will recursively call itself on the
+     * "new" friends of the current Account, and return the largest clique of those
+     * results.
+     * @param currentNode  the current Account that is being analyzed
+     * @param visitedNodes the visited Accounts that are all part of a clique
+     * @return             a {@link ArrayList<Account>} of the biggest clique
+     */
     private ArrayList<Account> analyzeNode (Account currentNode, ArrayList<Account> visitedNodes) {
         int matchedNodes = 0;
         ArrayList<Account> unvisitedNodes = new ArrayList<Account>();
@@ -115,6 +174,11 @@ public abstract class Account extends Page {
         return biggestClique;
     }
 
+    /**
+     * Determines if all the inputted Accounts are friends with one another
+     * @param set a group of Accounts that is potentially a clique
+     * @return    boolean on whether the set is a clique or not
+     */
     public static boolean isClique (ArrayList<Account> set) {
         for (Account a : set) {
             int matches = 0;
